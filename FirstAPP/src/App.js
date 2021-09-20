b6a65e149b3b294eae3ac3817c157b71a7254a56
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-09-20 15:01:51 -0300

    add firts app

---
+++ FirstAPP/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>To vivo</Text>
+      </SafeAreaView>
+    );
+  }
+}
+export default App;
